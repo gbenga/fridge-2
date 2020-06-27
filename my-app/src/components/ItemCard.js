c36fa1261ae1d1e041eeeed5f20656d529ace630
gojoaromokudu <gojoaromokudu@gmail.com>
2020-06-27 14:33:43 +0100

    Added images to cards

--- my-app/src/components/ItemCard.js
+++ my-app/src/components/ItemCard.js
@@ -4,13 +4,21 @@ import { Header } from "semantic-ui-react";
 export default class ItemCard extends React.Component {
   render() {
     const i = this.props.item;
+    const foodAddress = i.name.toLowerCase().split(" ").join("-");
+    let itemImage = require(`../item-imgs/${foodAddress}.jpg`);
+
     return (
       <div>
-        <Header as="h2">{i.name}</Header>
+        <Header as="h3">{i.name}</Header>
+        <img src={itemImage} width="200px" />
       </div>
     );
   }
 }
+
+// itemImage={`../item-imgs/${item.name
+//   .toLowerCase()
+//   .replace(" ", "-")}.jpg`}
 
 // id: 1,
 // name: "Yoghurt",
