abe7210796828dc90225e395fb0acfac460571bb
Adao Natalino <adaojosen@hotmail.com>
2020-06-28 13:06:30 +0100

    arrow Functions

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,11 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import "semantic-ui-css/semantic.min.css";
-import { Button, Segment } from "semantic-ui-react";
+import { Segment } from "semantic-ui-react";
 import Menu from "./components/Menu";
 import Container from "./components/ItemsContainer";
 // import data from "./db";
 import API from "./API"
+
 
 export default class App extends Component {
   state = {
@@ -16,19 +17,27 @@ export default class App extends Component {
     API.getItems().then(array => this.setState({items: array}))
   }
 
-  updateFilter(e) {
-    console.log(e.target.value);
-    this.setState({ search: e.target.value }) 
+  updateFilter = (e) => {
+    this.setState({search: e.target.value})
+  }
+
+  filterBySearch = (arrayOfItems) => {
+    return arrayOfItems.filter(item => item.name.toLowerCase().includes(this.state.search.toLocaleLowerCase()))
+  }
+
+  itemToRender = () => {
+    const items = this.filterBySearch(this.state.items)
+    return items
   }
 
   render() {
     return (
-      <>
+      <Fragment>
         <Segment basic padded="very">
-          <Menu updateFilter={this.updateFilter} />
+          <Menu updateFilter={this.updateFilter}/>
         </Segment>
-        <Container items={this.state.items} />
-      </>
+        <Container items={this.itemToRender()} />
+      </Fragment>
     );
   }
 }
--- my-app/src/components/Menu.js
+++ my-app/src/components/Menu.js
@@ -1,50 +1,19 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Input, Menu } from "semantic-ui-react";
+export default class MenuExampleSecondary extends Component {
 
-export default class Menu extends Component {
   render() {
+
     return (
-      <>
-        <input type="text" onChange={this.props.updateFilter}/>
-      </>
-    )
+      <Menu secondary>
+        <Menu.Item name="home" />
+        <Menu.Menu position="right">
+          <Menu.Item>
+            <Input onChange={this.props.updateFilter} icon="search" placeholder="Search..." />
+          </Menu.Item>
+          <Menu.Item name="logout" />
+        </Menu.Menu>
+      </Menu>
+    );
   }
 }
-
-
-// import React, { Component } from "react";
-// import { Input, Menu } from "semantic-ui-react";
-
-// export default class MenuExampleSecondary extends Component {
-//   // state = { activeItem: "home"};
-
-//   // searchFunction(value){
-//   //   this.props.updateState(value)
-//   //   // debugger
-//   // }
-
-//   // handleItemClick = (e, { name }) => this.setState({ activeItem: name });
-
-//   render() {
-//     // const { activeItem } = this.state;
-
-//     return (
-//       <Menu secondary>
-//         <Menu.Item
-//           name="home"
-//           // active={activeItem === "home"}
-//           // onClick={this.handleItemClick}
-//         />
-//         <Menu.Menu position="right">
-//           <Menu.Item>
-//             <Input onChange={this.props.updateState} icon="search" placeholder="Search..." />
-//           </Menu.Item>
-//           <Menu.Item
-//             name="logout"
-//             // active={activeItem === "logout"}
-//             // onClick={this.handleItemClick}
-//           />
-//         </Menu.Menu>
-//       </Menu>
-//     );
-//   }
-// }
